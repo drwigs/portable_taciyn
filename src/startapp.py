ebd27856886616f4f9d61b855c29d4d10c7ba525
Wigs2101 <100806795+drwigs@users.noreply.github.com>
2024-06-02 11:06:10 -0700

    Update startapp.py

--- src/startapp.py
+++ src/startapp.py
@@ -1,4 +1,5 @@
 import os
+import platform
 
 from .updates import manage_updates
 
@@ -7,9 +8,13 @@ GIT_UPDATE_CONFIGURATION = 'https://raw.githubusercontent.com/drwigs/portable_ta
 def run_app(app_name):
     if app_name+".py" in os.listdir('apps/'):
         manage_updates('apps', GIT_UPDATE_CONFIGURATION, False)
-        os.system("python3 apps/multiop.py")
+        if platform.system() == 'Linux':
+            os.system("clear")
+    
+        else:
+            os.system("cls")
+            os.system("python3 apps/multiop.py")
         print("Programa "+app_name+" finalizado")
     
     else:
-        print(os.listdir('apps/'))
         print("Programa no encontrado.")
